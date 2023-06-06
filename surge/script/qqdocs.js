36ff457e8ea9fdf97e39dad404993b3be1a632f9
Jianyun <yihy8023@gmail.com>
2023-06-06 09:55:14 +0800

    腾讯文档链接重定向

--- surge/script/qqdocs.js
+++ surge/script/qqdocs.js
@@ -1,8 +1,15 @@
 // 腾讯文档链接重定向 = type=http-request,pattern=^https\:\/\/docs.qq.com\/scenario\/link.html\?url=,script-path=https://github.com/jianyun8023/SnippetsVault/raw/main/surge/script/qqdocs.js,requires-body=false
 
 
+function getQueryString(url, name) {
+    const searchParams = new URLSearchParams(url.split('?')[1]);
+    return searchParams.get(name);
+}
+
 // 提取链接中的url参数
-const urlParam = decodeURIComponent($request.url);
+const url = decodeURIComponent($request.url);
+
+const urlParam = getQueryString(url, 'url');
 
 console.log("url 参数" + urlParam);
 
