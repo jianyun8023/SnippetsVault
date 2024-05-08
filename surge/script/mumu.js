5c938c560352530d582d17e4832ebe370a87f8ad
Jianyun <yihy8023@gmail.com>
2024-05-08 16:29:48 +0800

    update

--- surge/script/mumu.js
+++ surge/script/mumu.js
@@ -9,17 +9,19 @@ console.log($response.body)
 let obj = JSON.parse($response.body);
 
 if (url.includes("/api/v1/user/info") || url.includes("api/v1/login/by_mobile")) {
-    // obj.data.current_device.trial_end_at = end_at
+    obj.data.current_device.trial_status = 1
+    obj.data.current_device.trial_end_at = end_at
     // obj.data.member_status = 1
     // obj.data.member_expired_at = end_at
 } else if (url.includes("/api/v1/device/current/trial")) {
     obj.data.trial_end_at = end_at
-} else if (url.includes("/api/mac/pro/appcast/force")) {
-    obj.items = []
 } else if (url.includes("/api/mac/pro/appcast/remind")) {
     obj.items = []
+} else if (url.includes("/api/mac/pro/appcast/force")) {
+    obj.items = []
+} else if (url.includes("/api/v1/device/config")) {
+    obj.data.free_trial_days = 9999
 } 
-
 
 if (isQuanX) {
     $done({ body: JSON.stringify(obj) });
