9386fc26f8d75356ab70d497d3c8977a56230924
Jianyun <yihy8023@gmail.com>
2024-05-08 16:24:41 +0800

    test mumu

--- surge/script/mumu.js
+++ surge/script/mumu.js
@@ -5,12 +5,13 @@ const isSurge = typeof $httpClient != "undefined" && !isLoon
 let end_at = 2516312087
 
 if (!$response.body) $done({});
+console.log($response.body)
 let obj = JSON.parse($response.body);
 
 if (url.includes("/api/v1/user/info") || url.includes("api/v1/login/by_mobile")) {
-    obj.data.current_device.trial_end_at = end_at
-    obj.data.member_status = 1
-    obj.data.member_expired_at = end_at
+    // obj.data.current_device.trial_end_at = end_at
+    // obj.data.member_status = 1
+    // obj.data.member_expired_at = end_at
 } else if (url.includes("/api/v1/device/current/trial")) {
     obj.data.trial_end_at = end_at
 } else if (url.includes("/api/mac/pro/appcast/force")) {
